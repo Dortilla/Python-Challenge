8df2753fc8cea1cdd0878437cd4179a336d64496
Jae Neuharth <Jae.neuharth@gmail.com>
2022-07-12 22:39:09 -0500

    Finished both scripts, added comments to each.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,12 @@
 import os
 import csv
 
-budget_csv = os.path.join("Resources", "budget_data.csv" )
+budget_csv = os.path.join("Resources", "budget_data.csv" ) # assign path of file to 'budget_csv'
 
-date = []
+date = [] #variables 
 revenue = [] 
 revenue_change = []
+
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
@@ -19,7 +20,8 @@ with open(budget_csv) as csvfile:
         revenue_change.append(revenue[i] - revenue[i-1])
     
         avg_revenue_change = sum(revenue_change) / len(revenue_change)
-    
+
+        rounded_average_revenue = round(avg_revenue_change , 2)
         max_revenue_change = max(revenue_change)
     
         min_revenue_change = min(revenue_change)
@@ -27,23 +29,16 @@ with open(budget_csv) as csvfile:
         max_revenue_change_date = str(date[revenue_change.index(max(revenue_change))])
         min_revenue_change_date = str(date[revenue_change.index(min(revenue_change))])
 
-
-
-print("Financial Analysis")
-print("------------------")
-print("Total Months: ", len(date))
-print("Total: ", sum(revenue))
-print("Average Change: " , avg_revenue_change)
-print("Greatest Increase in Profits: " , max_revenue_change_date, "(" , max_revenue_change , ")")
-print("Greatest Decrease in Profits: " , min_revenue_change_date , "(" , min_revenue_change , ")")
+toprint = f"""Financial Analysis
+------------------
+Total Months:  {len(date)}
+Total: ${round(sum(revenue))}
+Average Change: ${round(avg_revenue_change, 2)}
+Greatest Increase in Profits: {max_revenue_change_date} (${max_revenue_change}) 
+Greatest Decrease in Profits: {min_revenue_change_date} (${min_revenue_change}) """
+print(toprint)
 
 resultspath = os.path.join("Analysis" , "results.txt" )
 with open(resultspath , "w") as external_file: 
-    print("Financial Analysis" , file=external_file)
-    print("------------------", file=external_file)
-    print("Total Months: ", len(date), file=external_file)
-    print("Total: ", sum(revenue), file=external_file)
-    print("Average Change: " , avg_revenue_change, file=external_file)
-    print("Greatest Increase in Profits: " , max_revenue_change_date, "(" , max_revenue_change , ")", file=external_file)
-    print("Greatest Decrease in Profits: " , min_revenue_change_date , "(" , min_revenue_change , ")", file=external_file)
+ external_file.writelines(toprint)
 external_file.close()
